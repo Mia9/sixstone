15e929e9360957534b7200c4f94907024e3e713d
Lim <limhsiuying90@gmail.com>
2023-10-23 16:05:34 +0800

    add edit on product show.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,19 @@ class ProductsController < ApplicationController
 		@product = Product.find(params[:id])
 	end
 
+	def edit
+		@product = Product.find(params[:id])	
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		@product.update(product_params)
+		if @product.save
+			redirect_to product_path(@product)
+		else
+			render :edit
+		end
+	end
 
 	def product_params
 		params.require(:product).permit(:name, :category, :description, :price, :quantity)
