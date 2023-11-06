6b5ae703d62bd4099a8c65f71d5914834808f764
Lim <limhsiuying90@gmail.com>
2023-11-06 08:48:04 +0800

    add authenticate users properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-	before_action :extract_shopping_cart
+	before_action :authenticate_user!
+  before_action :extract_shopping_cart
 
 	helper_method :admin?
     def admin?
